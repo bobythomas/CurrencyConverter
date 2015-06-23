66ade748ccce82b3f10efc293edbd9d27d82a261
bobythomas <bobythomas@bobys-MacBook-Pro.local>
2015-06-23 01:05:14 -0400

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by boby thomas on 2015-06-23.
+//  Copyright (c) 2015 boby thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
